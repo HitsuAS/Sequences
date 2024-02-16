e2afc6566263038e742db1dc190f20cba8aced3d
HitsuAS <abdallahshammout42@gmail.com>
2024-02-16 23:25:54 +0300

    Added QuestionsAndAnswersStorage

---
+++ app/src/main/java/com/example/sequences/model/QuestionsAndAnswersStorage.kt
@@ -0,0 +1,6 @@
+package com.example.sequences.model
+
+object QuestionsAndAnswersStorage {
+    val questions: MutableList<Question> = mutableListOf()
+    val answers: MutableList<Int> = mutableListOf()
+}
