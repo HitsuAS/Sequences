9bd5e21262c749a5fdf8de7c7df4f4eaf31a80b3
HitsuAS <abdallahshammout42@gmail.com>
2024-02-21 20:37:51 +0300

    Added Sequences logo to the TopAppBar

--- app/src/main/java/com/example/sequences/MainActivity.kt
+++ app/src/main/java/com/example/sequences/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -42,6 +44,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -115,7 +118,19 @@ fun SequencesGame(modifier: Modifier = Modifier) {
 @Composable
 fun SequencesTopAppBar(label: String) {
     CenterAlignedTopAppBar(
-        title = { Text(text = label) },
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(64.dp),
+                    painter = painterResource(id = R.drawable.sequences_logo),
+                    contentDescription = null
+                )
+                Text(text = label)
+            }
+        },
         colors = TopAppBarDefaults.topAppBarColors(containerColor = MaterialTheme.colorScheme.background)
     )
 }
